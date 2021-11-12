0dbb0a55a61f04f69249a68b06192afda55de915
Nguyen Thanh Luan <luann4099@gmail.com>
2021-11-12 20:44:35 +0700

    demo linear-layouyt :

---
+++ app/src/main/java/com/example/bkd0708k11/ActivitySignup.java
@@ -0,0 +1,14 @@
+package com.example.bkd0708k11;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivitySignup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
