3cf1487e11740e5e0de781429387a83eb5abe802
Nguyen Thanh Luan <luann4099@gmail.com>
2021-12-01 20:05:25 +0700

    update

--- app/src/test/java/com/example/bkd0708k11/ExampleUnitTest.java
+++ app/src/test/java/com/example/bkd0708k11/ExampleUnitTest.java
@@ -1,5 +1,9 @@
 package com.example.bkd0708k11;
 
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,8 +14,36 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+    Calculator calculator;
+    @Before
+    public void setUp() throws Exception {
+        System.out.println("before");
+        calculator = new Calculator();
+    }
+
     @Test
     public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+        System.out.println("test 1");
+        assertEquals(4, calculator.plus(2,2));
     }
+
+    @Test
+    public void sub_isCorrect() {
+        System.out.println("test 2");
+        assertEquals(2, calculator.sub(4,2));
+    }
+
+
+    @Test
+    public void isThrowException() {
+        assertThrows(Exception.class, () -> {
+            Integer.parseInt("1");
+        });
+    }
+
+    @Test
+    public void isEqualArr() {
+        assertArrayEquals(new int[]{2, 3, 4}, new int[]{2, 3, 4});
+    }
+
 }
