d90b7f8a270505f0e07aaf43577c807b89613857
Nguyen Thanh Luan <luann4099@gmail.com>
2021-12-01 20:53:21 +0700

    exercise-6

--- app/src/main/java/com/example/bkd0708k11/MainActivity.java
+++ app/src/main/java/com/example/bkd0708k11/MainActivity.java
@@ -6,29 +6,67 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText edtShowResult;
+    String numberOne = "";
+    String numberTwo = "";
+    String operationName = null;
+    Float result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        test1();
-
-        System.out.println("hello");
-
-        test2();
+        edtShowResult = findViewById(R.id.edtShowResult);
     }
 
-    public void test1() {
-        for (int i = 0; i < 5; i++) {
-            System.out.println(i);
+    public void getButtonNumberValue(View view) {
+        Button btn = (Button) view;
+        String number = btn.getText().toString();
+
+        if (this.operationName == null) {
+            this.numberOne = this.numberOne + number;
+        } else {
+            this.numberTwo = this.numberTwo + number;
         }
     }
 
-    public void test2() {
-        for (int j = 0; j < 5; j++) {
-            System.out.println(j);
+    public void getOperationName(View view) {
+        Button btn = (Button) view;
+        this.operationName = btn.getText().toString();
+    }
+
+
+    public void clear(View view) {
+        this.numberOne = "";
+        this.numberTwo = "";
+        this.result = null;
+        edtShowResult.setText("");
+        this.operationName = null;
+    }
+
+    public void calc(View view) {
+        switch (this.operationName) {
+            case "+":
+                this.result = Float.parseFloat(this.numberOne) + Float.parseFloat(this.numberTwo);
+                break;
+
+            case "-":
+                this.result = Float.parseFloat(this.numberOne) - Float.parseFloat(this.numberTwo);
+                break;
+
+            case "*":
+                this.result = Float.parseFloat(this.numberOne) * Float.parseFloat(this.numberTwo);
+                break;
+
+            case "/":
+                this.result = Float.parseFloat(this.numberOne) / Float.parseFloat(this.numberTwo);
+                break;
         }
+
+        edtShowResult.setText(String.format(this.result + ""));
     }
 }
