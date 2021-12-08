7bf7c92fe3a36855a7914e1193a4f1ec247f7bb4
Nguyen Thanh Luan <luann4099@gmail.com>
2021-12-08 19:55:35 +0700

    demo checkbox

---
+++ app/src/main/java/com/example/bkd0708k11/Session9.java
@@ -0,0 +1,58 @@
+package com.example.bkd0708k11;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+
+import java.util.ArrayList;
+
+public class Session9 extends AppCompatActivity {
+    ArrayList<String> checkedHobbies = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_session9);
+    }
+
+    public void onCheckHandler(View view) {
+        CheckBox checkBox = (CheckBox) view;
+        boolean isChecked = checkBox.isChecked();//cho biết là checkbox đc check hay là không
+        switch (view.getId()) {
+            case R.id.cboGame:
+                //xử lý trong trưường hợp check/uncheck game
+                if (isChecked) {
+                    checkedHobbies.add("game");
+                }else{
+                    checkedHobbies.remove("game");
+                }
+                break;
+
+            case R.id.cboCoding:
+                //xử lý trong trường hợp check/uncheck coding
+                if (isChecked) {
+                    checkedHobbies.add("coding");
+                }else{
+                    checkedHobbies.remove("coding");
+                }
+                break;
+
+            case R.id.cboBoxing:
+                //xử lý trong trường hợp check/uncheck boxing
+                if (isChecked) {
+                    checkedHobbies.add("boxing");
+                }else{
+                    checkedHobbies.remove("boxing");
+                }
+                break;
+        }
+    }
+
+    public void getCheckedResult(View v) {
+        for (int i = 0; i < checkedHobbies.size() ; i++) {
+            System.out.println(checkedHobbies.get(i));
+        }
+    }
+}
