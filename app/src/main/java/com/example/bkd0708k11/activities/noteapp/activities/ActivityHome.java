4c789396a55ea115fbda233456daccd73934aabf
Nguyen Thanh Luan <luann4099@gmail.com>
2022-03-02 20:15:10 +0700

    init code

---
+++ app/src/main/java/com/example/bkd0708k11/activities/noteapp/activities/ActivityHome.java
@@ -0,0 +1,16 @@
+package com.example.bkd0708k11.activities.noteapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.bkd0708k11.R;
+
+public class ActivityHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
